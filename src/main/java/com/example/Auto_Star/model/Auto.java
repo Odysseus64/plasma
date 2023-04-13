8205fd85cc7e770254872252be435bfdec0d7a72
Neon <jotarokudjo01@gmail.com>
2023-04-13 15:03:05 +0600

    Update

--- src/main/java/com/example/Auto_Star/model/Auto.java
+++ src/main/java/com/example/Auto_Star/model/Auto.java
@@ -2,11 +2,6 @@ package com.example.Auto_Star.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,19 +22,4 @@ public class Auto {
     private String owner;
     @Column(name = "Price")
     private int price;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "auto")
-    private static List<Image> images = new ArrayList<>();
-    private Long previewImageId;
-    private LocalDateTime dateOfCreate;
-
-    @PrePersist
-    public void init() {
-        dateOfCreate = LocalDateTime.now();
-    }
-
-    public void addImageToAuto(Image image) {
-        image.setAuto(this);
-        images.add(image);
-    }
 }
--- src/main/java/com/example/Auto_Star/servie/AutoService.java
+++ src/main/java/com/example/Auto_Star/servie/AutoService.java
@@ -1,14 +1,11 @@
 package com.example.Auto_Star.servie;
 
 import com.example.Auto_Star.model.Auto;
-import com.example.Auto_Star.model.Image;
 import com.example.Auto_Star.repository.AutoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -23,36 +20,8 @@ public class AutoService {
         return autoRepository.findAll();
     }
 
-    public void saveProduct(Auto auto, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
-        Image image1;
-        Image image2;
-        Image image3;
-        if (file1.getSize() != 0) {
-            image1 = toImageEntity1(file1);
-            auto.addImageToAuto(image1);
-        }
-        if (file2.getSize() != 0) {
-            image2 = toImageEntity1(file2);
-            auto.addImageToAuto(image2);
-        }
-        if (file3.getSize() != 0) {
-            image3 = toImageEntity1(file3);
-            auto.addImageToAuto(image3);
-        }
-        log.info("Saving new {}", auto);
-        Auto auto1 = autoRepository.save(auto);
-//        auto1.setPreviewImageId(auto1.get(0).getId());
+    public void saveProduct(Auto auto){
         autoRepository.save(auto);
-    }
-
-    private Image toImageEntity1(MultipartFile file1) throws IOException {
-        Image image = new Image();
-        image.setName(file1.getName());
-        image.setOriginalFileName(file1.getOriginalFilename());
-        image.setSize(file1.getSize());
-        image.setBytes(file1.getBytes());
-        image.setContentType(file1.getContentType());
-        return image;
     }
 
     public void deleteProduct(Long id) {
