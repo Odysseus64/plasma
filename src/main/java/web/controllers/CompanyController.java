63cc1891225f312fa5e3aa80941dbf293d1e8c39
Neon <jotarokudjo01@gmail.com>
2023-04-16 01:01:17 +0600

    Спать завтра продолжим

--- src/main/java/web/controllers/CompanyController.java
+++ src/main/java/web/controllers/CompanyController.java
@@ -4,8 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import web.models.Company;
 import web.servie.CompanyService;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -13,4 +13,9 @@ import web.servie.CompanyService;
 public class CompanyController {
     private final CompanyService service;
 
+    @GetMapping("/get/all")
+    public String getAll(Model model) {
+        model.addAttribute("all", service.findAll());
+        return "/company/company-list";
+    }
 }
