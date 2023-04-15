16c00b67bc3dd50b4a73f08eaf5ba74d09229f51
Neon <jotarokudjo01@gmail.com>
2023-04-15 14:40:47 +0600

    Дописал services

--- src/main/java/web/servie/StudentService.java
+++ src/main/java/web/servie/StudentService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import web.models.Student;
+import web.models.StudyFormat;
+import web.repository.StudentRepository;
 import web.servie.metods.StudentMethods;
 
 import java.util.List;
@@ -12,28 +14,39 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class StudentService implements StudentMethods {
+    private final StudentRepository sRepo;
+
     @Override
     public Student save(Student student) {
-        return null;
+        log.info("Student saved {}", student);
+        return sRepo.save(student);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        log.info("Student deleted {}", id);
+        sRepo.deleteById(id);
     }
 
     @Override
     public Student findById(Long id) {
-        return null;
+        log.info("Student found {}", id);
+        return sRepo.findById(id).orElse(null);
     }
 
     @Override
     public List<Student> findAll() {
-        return null;
+        return sRepo.findAll();
     }
 
     @Override
     public void update(Student student, Long id) {
-
+        log.info("The student has been successfully update {}", student);
+        Student student1 = sRepo.getById(id);
+        student1.setEmail(student.getEmail());
+        student1.setFirst_name(student.getFirst_name());
+        student1.setLast_name(student.getLast_name());
+        student1.setStudyFormat(StudyFormat.ONLINE);
+        sRepo.save(student1);
     }
 }
--- src/main/java/web/servie/TeacherService.java
+++ src/main/java/web/servie/TeacherService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import web.models.Teacher;
+import web.repository.TeacherRepository;
 import web.servie.metods.TeacherMethods;
 
 import java.util.List;
@@ -12,28 +13,38 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TeacherService implements TeacherMethods {
+    private final TeacherRepository tRepo;
+
     @Override
     public Teacher save(Teacher teacher) {
-        return null;
+        log.info("Teacher saved {}", teacher);
+        return tRepo.save(teacher);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        log.info("Teacher deleted {}", id);
+        tRepo.deleteById(id);
     }
 
     @Override
     public Teacher findById(Long id) {
-        return null;
+        log.info("Teacher found {}", id);
+        return tRepo.findById(id).orElse(null);
     }
 
     @Override
     public List<Teacher> findAll() {
-        return null;
+        return tRepo.findAll();
     }
 
     @Override
     public void update(Teacher teacher, Long id) {
-
+        log.info("The teacher had been successfully update {}", teacher);
+        Teacher teacher1 = tRepo.getById(id);
+        teacher1.setEmail(teacher.getEmail());
+        teacher1.setFirst_name(teacher.getFirst_name());
+        teacher1.setLast_name(teacher.getLast_name());
+        tRepo.save(teacher1);
     }
 }
