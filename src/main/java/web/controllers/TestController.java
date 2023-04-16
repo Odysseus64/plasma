99d72725f5a275aad5b4ba850b228a51b92da900
Neon <jotarokudjo01@gmail.com>
2023-04-16 22:05:39 +0600

    CRUD 0.2.5

--- src/main/java/web/controllers/TestController.java
+++ src/main/java/web/controllers/TestController.java
@@ -22,25 +22,22 @@ public class TestController {
         return "/test/create-test";
     }
 
-    @GetMapping("/get/{id}")
-    public String getById(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("prom", testService.getTestById(id));
+    @GetMapping("/get/all")
+    public String getById(Model model) {
+        model.addAttribute("prom", testService.findAll());
         return "/test/view-test";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        TestModel test = new TestModel();
+        model.addAttribute("test", test);
+        return "/test/create-test";
     }
 
     @PostMapping("/create")
     public String createProduct(TestModel test) {
         testService.saveTest(test);
-        return "redirect:/testing/";
-    }
-
-    @GetMapping("/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
-        TestModel test = testService.getTestById(id);
-        if (test != null) {
-            model.addAttribute("test", test);
-            return "/test/update-test";
-        }
         return "redirect:/testing/";
     }
 
@@ -53,19 +50,22 @@ public class TestController {
         existingTest.setModel(test.getModel());
         existingTest.setPrice(test.getPrice());
         testService.saveTest(existingTest);
-        return "redirect:/testing/get/{id}";
+        return "redirect:/testing/get/all";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        TestModel test = testService.getTestById(id);
+        if (test != null) {
+            model.addAttribute("test", test);
+            return "/test/update-test";
+        }
+        return "redirect:/testing/";
     }
 
     @PostMapping("/delete/{id}")
     public String deleteById(@PathVariable("id") Long id) {
         testService.deleteTest(id);
-        return "redirect:/testing/";
-    }
-
-    @GetMapping("/create")
-    public String showCreateForm(Model model) {
-        TestModel test = new TestModel();
-        model.addAttribute("test", test);
-        return "/test/create-test";
+        return "redirect:/testing/get/all";
     }
 }
