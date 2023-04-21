f5e2805fbb39f7ee1c08fb385dc949e5f35a6ac2
Neon <jotarokudjo01@gmail.com>
2023-04-21 21:21:33 +0600

    GPT chat wrote css

--- src/main/java/web/AutoStarApplication.java
+++ src/main/java/web/AutoStarApplication.java
@@ -9,4 +9,8 @@ public class AutoStarApplication {
     public static void main(String[] args) {
         SpringApplication.run(AutoStarApplication.class, args);
     }
+    /*
+    Привет! Это Neon, если ты фронтендшик напиши пожалуйста для моего сайта CSS и анимацию крутую
+    ахха спасибо )
+    */
 }
